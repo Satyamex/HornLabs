4c4177cf350e97835f442cb84c2c7e80906826d6
TheLostEco <thelosteco78@gmail.com>
2025-12-20 16:16:09 +0530

    gun placement and pickup fixed

--- prefab_scripts/player_interaction.gd
+++ prefab_scripts/player_interaction.gd
@@ -1,11 +1,11 @@
 extends Node3D
 
 @onready var interact_ray: RayCast3D = $"../cam/InteractRay"
-@onready var horn_gun: Node3D = $horn_gun
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("interact") and interact_ray.is_colliding():
 		var target: Node3D = interact_ray.get_collider()
 		if target.is_in_group("pickable"):
 			target.queue_free()
-			horn_gun.visible = true
+			$".".show()
+			$"../../AnimationPlayer".play("pickup")
